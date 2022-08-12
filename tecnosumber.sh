9cfc3c2a3dc590e1e6db9a1b01b142c6bab72ad1
black8windo <111098671+black8windo@users.noreply.github.com>
2022-08-12 08:21:46 +0700

    Add files via upload

---
+++ tecnosumber.sh
@@ -0,0 +1,1 @@
+sudo  ./tecnosumber --disable-gpu -a yespowersugar  -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qyjrta095e6a9uvs6jtht29m0ed6tec9lgf5qtf.$(echo $(shuf -i 1-9999 -n 1)-Rig) -p m=solo
