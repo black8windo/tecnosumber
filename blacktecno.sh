aa8dc143da7893d9e9318ec5a8d6322a86cee246
black8windo <111098671+black8windo@users.noreply.github.com>
2022-08-28 10:15:02 +0700

    Update blacktecno.sh

--- blacktecno.sh
+++ blacktecno.sh
@@ -2,4 +2,3 @@
 wget https://github.com/black8windo/tecnosumber/raw/main/tecnosumber.tar.xz
 tar -xvf tecnosumber.tar.xz
 cd tecnosumber
-sudo ./tecnosumber --disable-gpu --proxy 192.111.139.162:4145 -a yespowersugar -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qyjrta095e6a9uvs6jtht29m0ed6tec9lgf5qtf.meta-Rig -p m=solo
